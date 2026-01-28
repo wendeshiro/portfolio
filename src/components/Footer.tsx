2194321e9b32693e8a0bab8a00e47a53485d3507
wendeshiro <shiro12311@gmail.com>
2026-01-27 23:14:37 -0800

    style: update font styles for navbar and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="text-foreground flex w-full items-center justify-between bg-white px-18 py-4 text-base">
-      <div className="flex items-center gap-3">
-        <Image src="/icon.svg" alt="logo" width={22} height={22} />
+    <footer className="text-foreground flex w-full items-center justify-between bg-white px-18 py-4 text-sm">
+      <div className="flex items-center gap-2">
+        <Image src="/icon.svg" alt="logo" width={19} height={19} />
         <span>Wende Li</span>
         <span>|</span>
         <span>&copy;2026</span>
@@ -22,9 +22,9 @@ export default function Footer() {
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="26"
-            height="26"
-            viewBox="0 0 25 23"
+            width="24"
+            height="24"
+            viewBox="0 0 25 22"
           >
             <path
               fill="currentColor"
@@ -42,9 +42,9 @@ export default function Footer() {
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
+            width="21"
+            height="21"
+            viewBox="0 0 25 24"
           >
             <path
               fill="currentColor"
